9c66f63aed44839969acf3bd84101b6bdfc39721
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-02-05 22:15:48 +0100

    use express

--- openclassrooms/backend/app.js
+++ openclassrooms/backend/app.js
@@ -1,9 +1,32 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.use((request, response) => {
+app.use((request, response, next) => {
+    console.log('Requête reçue.');
+    next();
+});
+
+app.use((request, response, next) => {
+    response.status(201);
+    next();
+});
+
+app.use((request, response, next) => {
     response.json({ message: 'Hello World!' });
+    next();
+});
+
+app.use((request, response, next) => {
+    console.log('La réponse a été envoyée.');
+    next();
+});
+
+app.use((request, response, next) => {
+    response.end(
+        'la réponse a déjà été envoyée, ce message ne sera pas affiché'
+    );
 });
 
 module.exports = app;
